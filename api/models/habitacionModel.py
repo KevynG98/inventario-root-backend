e746ba54f183c921a20e13c73135134cb121d157
Kevyn Giron <kgprogrammer98@gmail.com>
2025-05-25 23:18:11 -0600

    [FIX] Se arreglo el endpint de habitaciones para que haga el crud

--- api/models/habitacionModel.py
+++ api/models/habitacionModel.py
@@ -5,8 +5,9 @@ class Habitacion(models.Model):
     area = models.CharField(max_length=100)
     estado = models.CharField(max_length=100)
     admision = models.ForeignKey('Admision', on_delete=models.SET_NULL, null=True, blank=True, related_name='habitaciones_asignadas')
-    paciente = models.CharField(max_length=200, null=True, blank=True)  # solo el nombre o identificador
+    paciente = models.CharField(max_length=200, null=True, blank=True)
     nivel = models.CharField(max_length=50)
+    is_active = models.BooleanField(default=True)  # Soft delete
 
     def __str__(self):
         return f"{self.codigo} - {self.area}"
