0125557aeaff5a3affd75ba10414d3d44b03c5a7
KevynG98 <kgprogrammer98@gmail.com>
2025-06-09 18:21:46 -0600

    cambios de 9 nde junio

--- api/settings.py
+++ api/settings.py
@@ -28,13 +28,15 @@ if DEV:
 
     CSRF_TRUSTED_ORIGINS = [
         "http://localhost:3000",
-        "http://192.168.1.18:3000"
+        "http://192.168.1.18:3000",
+        "http://10.10.20.16:3000"
     ]
 
     CORS_ALLOWED_ORIGINS = [
         "http://localhost:3000",
         "http://127.0.0.1:3000",
-        "http://192.168.1.18:3000"
+        "http://192.168.1.18:3000",
+        "http://10.10.20.16:3000"
     ]
 
     DATABASES = {
