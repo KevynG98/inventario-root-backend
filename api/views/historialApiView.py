965921673eca32e9113922ba17b6c674984b74c8
Kevyn Giron <kgprogrammer98@gmail.com>
2025-06-21 20:11:55 -0600

    [FIX] Se optimizo la llamada a la db

--- api/views/historialApiView.py
+++ api/views/historialApiView.py
@@ -18,9 +18,11 @@ from ..models.inventariosSkuModel import InventarioSKU
 User = get_user_model()
 
 @api_view(['GET'])
-@permission_classes([IsAuthenticated])
+#@permission_classes([IsAuthenticated])
 def listar_historial_api(request):
-    queryset = HistorialAPI.objects.all().order_by('fecha')
+    queryset = HistorialAPI.objects.values(
+        'id', 'fecha', 'endpoint', 'descripcion', 'usuario__username', 'metodo', 'exito', 'codigo_respuesta'
+    )
 
     modulo = request.GET.get('modulo')
     if modulo:
@@ -34,24 +36,20 @@ def listar_historial_api(request):
         )
 
     fecha_inicio = request.GET.get('fecha_inicio')
-    if fecha_inicio:
+    fecha_fin = request.GET.get('fecha_fin')
+
+    if fecha_inicio and fecha_fin:
         try:
             inicio = datetime.strptime(fecha_inicio, '%Y-%m-%d')
-            queryset = queryset.filter(fecha__date__gte=inicio)
-        except ValueError:
-            return Response({'error': 'Formato de fecha_inicio inválido. Use YYYY-MM-DD.'}, status=400)
-
-    fecha_fin = request.GET.get('fecha_fin')
-    if fecha_fin:
-        try:
             fin = datetime.strptime(fecha_fin, '%Y-%m-%d') + timedelta(days=1)
-            queryset = queryset.filter(fecha__lt=fin)
+            queryset = queryset.filter(fecha__range=(inicio, fin))
         except ValueError:
-            return Response({'error': 'Formato de fecha_fin inválido. Use YYYY-MM-DD.'}, status=400)
+            return Response({'error': 'Fechas con formato inválido. Use YYYY-MM-DD.'}, status=400)
+
+    queryset = queryset.order_by('fecha')
 
     paginator = CustomPageNumberPagination()
     result_page = paginator.paginate_queryset(queryset, request)
-    serializer = HistorialAPISerializer(result_page, many=True)
 
     return Response({
         "count": paginator.page.paginator.count,
@@ -62,10 +60,9 @@ def listar_historial_api(request):
         "to": paginator.page.end_index(),
         "next": paginator.get_next_link(),
         "previous": paginator.get_previous_link(),
-        "results": serializer.data
+        "results": list(result_page),
     })
-
-
+    
 @api_view(['GET'])
 # @permission_classes([IsAuthenticated])
 def exportar_historial_pdf(request):
