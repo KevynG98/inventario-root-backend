bf105d261dccdf317e24b322a60f37ff878f7677
Kevyn Giron <kgprogrammer98@gmail.com>
2025-08-11 01:44:36 -0600

    [FIX] Se arreglo la unidad de medida

--- api/views/inventarioMedidaViews.py
+++ api/views/inventarioMedidaViews.py
@@ -5,7 +5,7 @@ from drf_yasg.utils import swagger_auto_schema
 
 from api.utils.pagination import CustomPageNumberPagination
 from ..models.inventarioMedidaModel import Medida
-from ..serializers.inventarioMarcaSerializer import MarcaSerializer  # 👈 CORREGIDO
+from ..serializers.inventarioMedidaSerializer import MedidaSerializer# 👈 CORREGIDO
 
 @swagger_auto_schema(method='get', tags=['Inventario - Medidas'], operation_description="Listar medidas activas con paginación")
 @api_view(['GET'])
@@ -13,21 +13,21 @@ def listar_medidas(request):
     medidas = Medida.objects.filter(is_active=True).order_by('id')
     paginator = CustomPageNumberPagination()
     result_page = paginator.paginate_queryset(medidas, request)
-    serializer = MarcaSerializer(result_page, many=True)
+    serializer = MedidaSerializer(result_page, many=True)
     return paginator.get_paginated_response(serializer.data)
 
 
-@swagger_auto_schema(method='post', tags=['Inventario - Medidas'], operation_description="Crear una nueva medida", request_body=MarcaSerializer)
+@swagger_auto_schema(method='post', tags=['Inventario - Medidas'], operation_description="Crear una nueva medida", request_body=MedidaSerializer)
 @api_view(['POST'])
 def crear_medida(request):
-    serializer = MarcaSerializer(data=request.data)
+    serializer = MedidaSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@swagger_auto_schema(method='put', tags=['Inventario - Medidas'], operation_description="Actualizar una medida por ID", request_body=MarcaSerializer)
+@swagger_auto_schema(method='put', tags=['Inventario - Medidas'], operation_description="Actualizar una medida por ID", request_body=MedidaSerializer)
 @api_view(['PUT'])
 def actualizar_medida(request, pk):
     try:
@@ -35,7 +35,7 @@ def actualizar_medida(request, pk):
     except Medida.DoesNotExist:
         return Response({'error': 'Medida no encontrada'}, status=status.HTTP_404_NOT_FOUND)
 
-    serializer = MarcaSerializer(medida, data=request.data)
+    serializer = MedidaSerializer(medida, data=request.data)
     if serializer.is_valid():
         serializer.save()
         return Response(serializer.data)
@@ -60,7 +60,7 @@ def eliminar_medida(request, pk):
 def obtener_medida(request, pk):
     try:
         medida = Medida.objects.get(pk=pk)
-        serializer = MarcaSerializer(medida)
+        serializer = MedidaSerializer(medida)
         return Response(serializer.data)
     except Medida.DoesNotExist:
         return Response({'error': 'Medida no encontrada'}, status=status.HTTP_404_NOT_FOUND)
